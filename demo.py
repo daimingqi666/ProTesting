04e6a3fa8a2b192562ecad9b08d164603c60c19f
daimingqi666 <576599875@qq.com>
2021-05-28 15:17:57 +0800

    20210528_first

---
+++ demo.py
@@ -0,0 +1,9 @@
+# from python.demo1 import demo1
+# from testing import demo2
+#
+# demo1()
+# print(demo2.hello)
+# demo2.f()
+
+from testing import *
+
---
+++ testing/demo2.py
@@ -0,0 +1,7 @@
+_all_ =[]
+hello ='hello demo2'
+
+def f():
+    print("demo2.py f()")
+class dmeo2:
+    pass
